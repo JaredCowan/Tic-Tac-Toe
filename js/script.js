cb315a8f9ab0540d33bacdd1ca41de974810ee08
JaredCowan <jaredlc@outlook.com>
2014-07-21 18:20:36 -0700

    starting to work on win logic

--- js/script.js
+++ js/script.js
@@ -21,14 +21,18 @@ function GameController($scope) {
     console.log(row, col);
     console.log($scope.currentPlayer)
 
-    /*
-
-    1. Get current player
-    2. Place currentPlayer's value (O or X) into the right col/row of board
-    3. Log out board value 
-    */
     
+    // var $scope.rowLast = $scope.board[0][0] + $scope.board[0][1] + $scope.board[0][2];
+    // $scope.rowLast.join().replace(/,/g, '');
     $scope.board[row][col] = $scope.currentPlayer;
     logBoard($scope.board);
+    if ($scope.board[0][0] + $scope.board[0][1] + $scope.board[0][2] == "XXX" || $scope.board[0][0] + $scope.board[0][1] + $scope.board[0][2] == "OOO" || $scope.board[0][0] + $scope.board[1][0] + $scope.board[2][0] == "XXX" || $scope.board[0][0] + $scope.board[1][0] + $scope.board[2][0] == "OOO" || $scope.board[0][1] + $scope.board[1][1] + $scope.board[2][1] == "XXX" || $scope.board[0][2] + $scope.board[1][0] + $scope.board[2][0] == "OOO") {
+      console.log('winner');
+    } else if ($scope.rowLast == "XXX") {
+      console.log('winnerwinner');
+    }
+    // else if ($scope.board[0][0].join() == "X,X,X" || $scope.board[1][0].join() == "X,X,X" || $scope.board[2][0].join() == "X,X,X") {
+    //   console.log('winner2');
+    // }
   };
 }
