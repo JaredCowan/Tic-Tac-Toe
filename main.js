efd4b8142f7f8e2965b4472873189f3c58c9cbed
JaredCowan <jaredlc@outlook.com>
2014-07-22 18:34:42 -0700

    commenting code and refactor

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-function TicTacController($scope) {
+var TicTacApp = angular.module('TicTacApp', ["firebase"]);
+TicTacApp.controller('TicTacController', function ($scope) {
 
+  // INITIATE GAME CELLS AND AND GRID LAYOUT
   var cells         = ['','','','','','','','',''];
-  var currentMark   =   'o';
-  var empty         =  true;
-  moves             =     1;
-  gameover          = false;
   grid              = [
       [ "" , "" , "" ],
       [ "" , "" , "" ],
       [ "" , "" , "" ]
   ];
-  $scope.leftScore  =      0;
-  $scope.rightScore =      0;
 
-  $scope.cells      =  cells;
-  $scope.showbtn    =  false;
+  // INITIATES GENERAL GAME BOOLEANS, VALUES AND VARIABLES FOR 'MOVES' 'GAMEOVER' 'SCORE' 'CELLS' 'EMPTY'
+  var currentMark   = 'o'; var empty         = true; moves         =      0; gameover       =  false;
+  $scope.leftScore  =   0; $scope.rightScore =    0; $scope.cells  =  cells; $scope.showbtn =  false;
 
+  // RESPONSIBLE FOR ASSIGING 'X' && 'O' VALUES TO BOARD AND RAISE 'MOVES' COUNT. CHECKS CURRENT PLAYER FOR NEXT TURN.
+  // DIVIDES 'GRID' AND 'CELLS' FOR INDEXES TO BE CHECKED FOR WINNING COMBINATIONS BY 'EVALUATEWIN()'.
   $scope.drawMark = function(index) {
     if (gameover == false && cells[index] == '') {
       if (currentMark == 'o') {
-        $scope.cells[index] = 'x';
-        currentMark         = 'x';
+        $scope.cells[index]     = 'x';
+        currentMark             = 'x';
+        moves                      ++;
       } else {
-        $scope.cells[index] = 'o';
-        currentMark         = 'o';
+        $scope.cells[index]     = 'o';
+        currentMark             = 'o';
+        moves                      ++;
       }
     }
     var row           =  Math.floor(index/3);
@@ -31,40 +32,22 @@ function TicTacController($scope) {
     grid[row][column] =          currentMark;
     if (gameover == false)     evaluateWin();
   }
-
+  // WIN/LOSE LOGIC - PRINTS MESSAGE ON RIGHT && LEFT PANEL FOR DRAWS. 'GAMEOVER' IS TRUE.
   var evaluateWin   = function() {
     if (grid[0][0] == "x" && grid[0][1] == "x" && grid[0][2] == "x" || grid[1][0] == "x" && grid[1][1] == "x" && grid[1][2] == "x" || grid[2][0] == "x" && grid[2][1] == "x" && grid[2][2] == "x" || grid[0][0] == "x" && grid[1][0] == "x" && grid[2][0] == "x" || grid[0][1] == "x" && grid[1][1] == "x" && grid[2][1] == "x" || grid[0][2] == "x" && grid[1][2] == "x" && grid[2][2] == "x" || grid[0][0] == "x" && grid[1][1] == "x" && grid[2][2] == "x" || grid[0][2] == "x" && grid[1][1] == "x" && grid[2][0] == "x") {
-      xwin();
-    } else if (grid[0][0] == "o" && grid[0][1] == "o" && grid[0][2] == "o" || grid[1][0] == "o" && grid[1][1] == "o" && grid[1][2] == "o" || grid[2][0] == "o" && grid[2][1] == "o" && grid[2][2] == "o" || grid[0][0] == "o" && grid[1][0] == "o" && grid[2][0] == "o" || grid[0][1] == "o" && grid[1][1] == "o" && grid[2][1] == "o" || grid[0][2] == "o" && grid[1][2] == "o" && grid[2][2] == "o" || grid[0][0] == "o" && grid[1][1] == "o" && grid[2][2] == "o" || grid[0][2] == "o" && grid[1][1] == "o" && grid[2][0] == "o") {
-      owin();
-    } else if (moves == 9) {
-      var messagebox  = document.getElementById('message');
-      $scope.leftMessage  = "draw...";
-      $scope.rightMessage = "draw...";
-    } else {
-      moves += 1;
+      xwin(); } else if     (grid[0][0] == "o" && grid[0][1] == "o" && grid[0][2] == "o" || grid[1][0] == "o" && grid[1][1] == "o" && grid[1][2] == "o" || grid[2][0] == "o" && grid[2][1] == "o" && grid[2][2] == "o" || grid[0][0] == "o" && grid[1][0] == "o" && grid[2][0] == "o" || grid[0][1] == "o" && grid[1][1] == "o" && grid[2][1] == "o" || grid[0][2] == "o" && grid[1][2] == "o" && grid[2][2] == "o" || grid[0][0] == "o" && grid[1][1] == "o" && grid[2][2] == "o" || grid[0][2] == "o" && grid[1][1] == "o" && grid[2][0] == "o") {
+      owin(); } else if (moves == 9) {
+      $scope.leftMessage  = "It's a draw..."; $scope.rightMessage = "It's a draw...";
+      var messagebox      =                       document.getElementById('message');
+      gameover            = true;
     }
   }
 
-  $scope.setName1      = function(player1Name) {
-    $scope.player1Name = '';
-  }
-
-  $scope.setName2      = function(player2Name) {
-    $scope.player2Name = '';
-  }
-
-  $scope.removeFocus1 = function(key) {
-    if (key.keyCode  == 13) {
-      key.target.blur();
-    }
-  }
-
-  $scope.removeFocus2 = function(key) {
-    if (key.keyCode  == 13) {
-      key.target.blur();
-    }
-  }
+  // DATABINDING AND FUNCTION FOR SETTING PLAYER1 AND PLAYER2 NAME && CSS DISABLE IF 'EMPTY' IS FALSE.
+  $scope.setName1      = function(player1Name)              { $scope.player1Name = ''; }
+  $scope.setName2      = function(player2Name)              { $scope.player2Name = ''; }
+  $scope.removeFocus1  = function(key) { if (key.keyCode  == 13) { key.target.blur(); }}
+  $scope.removeFocus2  = function(key) { if (key.keyCode  == 13) { key.target.blur(); }}
 
   var xwin = function () {
     $scope.leftMessage = $scope.player1Name + " wins!";
@@ -88,7 +71,7 @@ function TicTacController($scope) {
     $scope.rightMessage =    "";
     currentMark         =   'o';
     var empty           =  true;
-    moves               =     1;
+    moves               =     0;
     gameover            = false;
     grid                = [
         [ "" , "" , "" ],
@@ -96,7 +79,12 @@ function TicTacController($scope) {
         [ "" , "" , "" ]
     ];
   };
-
-}
-
+  // FIREBASE CHAT BOX ON GAME.HTML
+var myDataRef = new Firebase('https://tictactoe1.firebaseio.com/chat');
+  $('#messageInput').keypress(function (e) { if (e.keyCode == 13) { var name = $('#nameInput').val(); var text = $('#messageInput').val();
+      myDataRef.push({name: name, text: text}); $('#messageInput').val(''); }});
+      myDataRef.on('child_added', function(snapshot) { var message = snapshot.val(); displayChatMessage(message.name, message.text); });
+  function displayChatMessage(name, text) { $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight; };})
+// END FIREBASE CHAT BOX
 
